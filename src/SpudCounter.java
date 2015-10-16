4f175c4fcdcac0e761821008b768bb7cfc300da0
Henry Defibaugh <Guest@wts5.attlocal.net>
2015-10-15 18:25:58 -0700

    today we did a lot of four loops.

---
+++ src/SpudCounter.java
@@ -0,0 +1,13 @@
+public class SpudCounter {
+	public static void main(String[] args) {
+		for (int i = 1; i < 8; i++) {
+
+			if (i == 4) {
+				System.out.println(i);
+			} else {
+				System.out.println(i + " potato");
+			}
+		}
+		System.out.println("more");
+	}
+}
---
+++ src/bieberfever.java
@@ -0,0 +1,16 @@
+public class bieberfever {
+	public static void main(String[] args) {
+		for (int w = 0; w < 2; w++) {
+
+			for (int i = 0; i < 3; i++) {
+				System.out.println("baby");
+			}
+			if (w == 0) {
+				System.out.println("oh");
+			} else if (w == 1) {
+				System.out.println("no");
+			}
+		}
+		System.out.println("I thougth you would always be mine");
+	}
+}
