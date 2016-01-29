cb020c4a984e7bc121891bff43f60c592dae22b4
BigRockerEnding <bigrockerending@live.com>
2016-01-28 18:28:36 -0800

    Today we did Treasure Hunt.

--- src/TurtleTreasureHunt.java
+++ src/TurtleTreasureHunt.java
@@ -9,7 +9,10 @@ import org.teachingextensions.logo.ImageBackground;
 import org.teachingextensions.logo.Paintable;
 import org.teachingextensions.logo.Tortoise;
 
-/** Note: You will need the latest version of the TKP jar: http://school.wintrisstech.org/jars/TeachingKidsProgramming.jar **/
+/**
+ * Note: You will need the latest version of the TKP jar:
+ * http://school.wintrisstech.org/jars/TeachingKidsProgramming.jar
+ **/
 
 public class TurtleTreasureHunt implements KeyEventDispatcher {
 
@@ -17,53 +20,74 @@ public class TurtleTreasureHunt implements KeyEventDispatcher {
 
 	private void goUp() {
 		// 1. Make the tortoise move up the screen
-Tortoise.setAngle(0);
-Tortoise.move(100);
+		Tortoise.setAngle(0);
+		Tortoise.move(3);
 	}
 
 	private void goDown() {
 		// 2. make the tortoise move down the screen
-Tortoise.setAngle(180);
-Tortoise.move(100);
+		Tortoise.setAngle(180);
+		Tortoise.move(3);
+
 	}
 
 	private void goLeft() {
 		// 3. make the tortoise move left (3 lines of code)
 		// Hint: the turn() method lags more than setAngle()
+		Tortoise.setAngle(-90);
+		Tortoise.move(3);
 
-	
-	
 	}
 
 	private void goRight() {
 		// 4. make the tortoise move right
+		Tortoise.setAngle(90);
+		Tortoise.move(3);
+
 	}
 
 	private void spaceBarWasPressed() {
 		int tortoiseLocationX = Tortoise.getX();
 		int tortoiseLocationY = Tortoise.getY();
 
-		// 5. Print out the variables for tortoiseLocationX and tortoiseLocationY
-
+		// 5. Print out the variables for tortoiseLocationX and
+		// tortoiseLocationY
+		System.out.println(tortoiseLocationX);
+		System.out.println(tortoiseLocationY);
 		// 6. If tortoise is at same location as the little girl,
-		// 			make a pop-up tell the Tortoise where to go next
+		// make a pop-up tell the Tortoise where to go next
+		if (tortoiseLocationX <= 550 && tortoiseLocationX >= 450 && tortoiseLocationY >= 250
+				&& tortoiseLocationY <= 350) {
+
+			JOptionPane.showMessageDialog(null, "Now go and ask that little boy for a clue");
+		}
 
 		// 7. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
+		if (tortoiseLocationX >= 133 && tortoiseLocationX <= 153 && tortoiseLocationY >= 275
+				&& tortoiseLocationY <= 295) {
+			JOptionPane.showMessageDialog(null, "Now go ask the parrots for the last hint");
+		}
 
-		
+		if (tortoiseLocationX >= 412 && tortoiseLocationX <= 432 && tortoiseLocationY >= 98
+				&& tortoiseLocationY <= 118) {
+			JOptionPane.showMessageDialog(null, "You won!!!!");
+		}
 	}
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
 		/*
-		 * If you want to use your own background, download the image you want, and change the following line to point to it like: new
+		 * If you want to use your own background, download the image you want,
+		 * and change the following line to point to it like: new
 		 * ImageBackground("file:/Users/joonspoon/Desktop/dinosaurLand.jpg");
 		 */
-		Paintable backgroundImage = new ImageBackground("file:/Users/League/Google Drive/league-images/treasure_hunt.jpg");
+		Paintable backgroundImage = new ImageBackground(
+				"file:/Users/League/Google Drive/league-images/treasure_hunt.jpg");
 		Tortoise.getBackgroundWindow().addPaintable(backgroundImage);
 		Tortoise.penUp();
-		JOptionPane.showMessageDialog(null, "Ask the little girl for help with your quest. Press the space bar to ask.");
+		JOptionPane.showMessageDialog(null,
+				"Ask the little girl for help with your quest. Press the space bar to ask.");
 	}
 
 	public boolean dispatchKeyEvent(KeyEvent e) {
